7750e2aa5fb6ebc92a8188c4a0b7633d0d4dd6b8
agunbiadepaul <agunbiade_paul@yahoo.com>
2023-12-05 11:09:40 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * print_python_list_info -  function that prints some basic
  *							info about Python lists
