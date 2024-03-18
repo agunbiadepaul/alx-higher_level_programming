3ef25cfddba7fd178c732724f0277d555a0d6d3a
agunbiadepaul <agunbiade_paul@yahoo.com>
2024-03-18 11:29:11 -0700

    commiting

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-
-""" Accepts state name and displays data """
-
-from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    c = db.cursor()
-    q = 'SELECT * FROM states WHERE name = {} ORDER BY states.id ASC'.format(argv[4])
-    c.execute(q)
-    res = c.fetchall()
-    for r in res:
-        print(r)
-    c.close()
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
